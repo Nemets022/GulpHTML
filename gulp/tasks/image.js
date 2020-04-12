30725b29364784ddf8b002704ec74464d1699e1a
Nikita <Nemets022@gmail.com>
2020-04-12 11:20:03 +0300

    fix img

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
 const image = require('gulp-image')
 
 module.exports = imageTask = (cd) => {
-	return gulp.src('src/image/*')
+	return gulp.src('src/image/**/*')
 		.pipe(image())
 		.pipe(gulp.dest('build/img'))
 }
