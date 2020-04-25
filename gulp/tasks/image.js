31b98847c72ecd42d190ce57db5923d4ede56841
Nikita <Nemets022@gmail.com>
2020-04-25 14:49:42 +0300

    Add gulp-webp

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp')
 const image = require('gulp-image')
+const webp = require('gulp-webp')
 
 module.exports = imageTask = (cd) => {
 	return gulp.src('src/image/**/*')
 		.pipe(image())
+		.pipe(webp())
 		.pipe(gulp.dest('build/img'))
 }
